ade847a7e6e664d266c1be921eff17c890c52915
lishi <lishi1608@gmail.com>
2018-03-23 16:08:50 +0800

    no message

--- bin/globalsetting.js
+++ bin/globalsetting.js
@@ -1,7 +1,7 @@
 if(!process.env.STORJ_NETWORK) {
     process.env.STORJ_NETWORK = 'genaro';
 }
-console.log('you are on storage protocol: ' + process.env.STORJ_NETWORK)
+console.log('env.STORJ_NETWORK: ' + process.env.STORJ_NETWORK)
 Object.defineProperty(global, "_bitcore", {
     get: function(){
         return undefined;
